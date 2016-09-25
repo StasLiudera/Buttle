2eae6fa7a66596b9f6bb395a01a6caf3d378cc2b
stas.liudera <stasliudera@gmail.com>
2016-09-25 18:35:46 +0200

    Add max ship size 6 cells.

--- src/Field.java
+++ src/Field.java
@@ -10,15 +10,8 @@ public class Field {
 
 
     Field (int verticalSize, int horizontalSize, Hardness hardness) {
-        this.fieldVerticalSize = verticalSize;
-        this.fieldHorizontalSize = horizontalSize;
-        if (this.fieldVerticalSize < 2) {
-            this.fieldVerticalSize = 2;
-        }
-        if (this.fieldHorizontalSize < 2) {
-            this.fieldHorizontalSize = 2;
-        }
-
+        this.fieldVerticalSize = Math.max(verticalSize, 2);
+        this.fieldHorizontalSize = Math.max(horizontalSize, 2);
         this.coordinates = new Coordinate[this.fieldVerticalSize][this.fieldHorizontalSize];
         this.shipsCount = GetShipsCount(
                 Math.max(this.fieldVerticalSize, this.fieldHorizontalSize),
@@ -27,13 +20,16 @@ public class Field {
         );
 
         System.out.print("[");
+        int sum = 0;
         for (int i = 0; i < this.shipsCount.length; i++) {
             System.out.print(this.shipsCount[i]);
             if (i != this.shipsCount.length - 1) {
                 System.out.print(",");
             }
+            sum += this.shipsCount[i]*(i+1);
         }
-        System.out.print("]");
+        System.out.println("]");
+        System.out.print(sum);
     }
 
     private void FillField () {
@@ -55,13 +51,14 @@ public class Field {
 
     private int[] GetShipsCount (int maxShipSize, int coordinatesCount, Hardness hardness) {
 
+        maxShipSize = Math.min(6, maxShipSize);
         int[] shipsArray = new int[maxShipSize];
         int hardnessCoefficient = GetHardnessCoefficient(hardness);
         int realShipsArrayLength = 1;
         shipsArray[0] = coordinatesCount;
 
         while (shipsArray[0]-shipsArray[1]-3 >= hardnessCoefficient) {
-            for (int i = realShipsArrayLength; i >= 1; i--) {
+            for (int i = Math.min(realShipsArrayLength, shipsArray.length-1); i >= 1; i--) {
                 if (shipsArray[i-1]-shipsArray[i]-1 >= hardnessCoefficient && shipsArray[0]-i-1-shipsArray[1]-(i==1?1:0) >= hardnessCoefficient) {
                     shipsArray[i]++;
                     shipsArray[0] = shipsArray[0]-i-1;
